c0f1a9e473f40149279072e167be4cdc433e1cbd
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-02-05 11:07:43 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int hr1,min1,hr2,min2,hr,min;
+    scanf("%d %d",&hr1,&min1);
+    scanf("%d %d",&hr2,&min2);
+    hr=hr1-hr2;
+    min=min1-min2;
+    printf("%d %d",hr,min);
+}
