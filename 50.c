98ae2532f412fee7c482b84da46a994ce5a5b71f
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-02-26 16:17:15 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("enter the values of n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("it is a power of 2");
+    }
+    else
+    {
+        printf("it is not a power of 2");
+    }
+}
