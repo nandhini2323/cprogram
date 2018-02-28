46e03e1159079899505bd49b0ecf61191e15ebc5
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-02-28 15:54:26 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter two values");
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("odd");
+}
+}
